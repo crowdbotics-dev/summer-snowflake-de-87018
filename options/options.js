e928489f376b48ff7abc9b6a71e82a07585bfe99
Crowdbotics <engineering@crowdbotics.com>
2023-05-29 13:50:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "summer-snowflake-de-87018",
+  url: "https://summer-snowflake-de-87018.botics.co",
+  api: "https://summer-snowflake-de-87018.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
